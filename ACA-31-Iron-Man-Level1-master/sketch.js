3b3df4a0c26c37e8c18fd84512f74afa6009fe29
tejasbaseball <55719023+tejasbaseball@users.noreply.github.com>
2021-08-06 14:32:48 -0700

    aca32 commit

--- ACA-31-Iron-Man-Level1-master/sketch.js
+++ ACA-31-Iron-Man-Level1-master/sketch.js
@@ -1,9 +1,11 @@
 
 var bg, backgroundImg;
 var iron, ironImg;
+var stoneGroup, stoneImg;
 function preload() {
   backgroundImg = loadImage("images/bg.jpg");
   ironImg=loadImage("images/iron.png");
+  stoneImg=loadImage("images/stone.png");
 }
 
 function setup() {
@@ -13,34 +15,64 @@ function setup() {
   bg.scale=2;
   iron=createSprite(150,450,20,40);
   iron.addImage(ironImg);
-  iron.scale=0.4;
+  iron.scale=0.2;
   ground=createSprite(500,600,1000,10);
   ground.visible=false;
+  stoneGroup=new Group();
+  iron.setCollider("rectangle",100,0,200,400);
 }
 
 function draw() {
-  
- 
     if(keyDown("up"))
     {
       iron.velocityY=-10;
     }
     if(keyDown("left"))
     {
-      iron.velocityX=-5;
+      iron.x-=5;
     }
     if(keyDown("right"))
     {
-      iron.velocityX=5;
+      iron.x+=5;
     }
     if(bg.y<=0)
     {
       bg.y=500;
     }
+    if(iron.x<20||iron.x>950)
+    {
+      iron.x=150;
+    }
+    if(iron.y<50||iron.y>600)
+    {
+      iron.y=450;
+    }
     iron.velocityY+=0.5;
     bg.velocityY=-5;
     iron.collide(ground);
+    generateStones();
+    for(var i=0;i<stoneGroup.length;i++)
+    {
+        var temp=stoneGroup.get(i);
+        if(temp.isTouching(iron))
+        {
+            iron.collide(temp);
+        }
+    }
     drawSprites();
    
 }
+function generateStones()
+{
+  if(frameCount%70===0)
+    {
+        var stone=createSprite(500,-500,60,15);
+        stone.x=random(50,950);
+        stone.addImage(stoneImg);
+        stone.scale=0.3;
+        stone.velocityY=3;
+        stone.lifetime=750;
+        stoneGroup.add(stone);
 
+    }
+}
